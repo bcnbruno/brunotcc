caf3b2ac82c8674fc1c04182e7c56604bbf35c77
mhfribeiro <mhfribeiro@gmail.com>
2018-10-02 12:39:20 -0300

    Melhorias no arquivo flip.py

--- marcos/flip.py
+++ marcos/flip.py
@@ -3,17 +3,27 @@ import random
 s = 15
 n = 3
 
-sol = []
-for i in range(s):
-    sol.append(random.randint(0,1))
+# Array aleatório
+sol_orig = [random.randint(0,1) for i in range(s)]
 
-indexes = list(range(len(sol)))
+# Cópias (lembrar que a atribuição de listas em python é sempre referência, a cópia é feita por fatiamento)
+sol = sol_orig[:]
+sol_diff = sol[:]
+
+indexes = [i for i in range(s)]
     
-flip_indexes = random.choices(indexes, k=n)
+flip_indexes = random.sample(indexes, k=n)
 
-print(' Solução antes:', sol)
+print('Operações do Flip:')
 for i in flip_indexes:
-    sol[i] = int(not bool(sol[i]))
+    sol[i] = int(not bool(sol_orig[i]))
+    print('i = %d; de: %d, para: %d' % (i, sol_orig[i], sol[i]))
 
+for i in range(len(sol)):
+    sol_diff[i] = int(sol_orig[i] != sol[i])
+
+print('\n       Indexes:', indexes)
+print(' Solução antes:', sol_orig)
 print('Solução depois:', sol)
+print('     Diferença:', sol_diff)
 
