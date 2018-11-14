45b68d595ab59b9155b6ffd85765bbd4376db51e
bcnbruno <b_cnbruno@hotmail.com>
2018-11-14 15:38:11 -0200

    alterações no script.sh

--- bruno/script.sh
+++ bruno/script.sh
@@ -9,16 +9,16 @@
 ./ga_setpack.py --dt='2' --generation_size='50' --time='7200' --seed='150' solutions/ga/moba/solution8.csv
 ./ga_setpack.py --dt='2' --generation_size='50' --time='7200' --seed='200' solutions/ga/moba/solution9.csv
 ./ga_setpack.py --dt='2' --generation_size='50' --time='7200' --seed='1000' solutions/ga/moba/solution10.csv
-./ga_setpack.py --k='5' --dt='3' --generation_size='50' --time='7200' --seed='1' solutions/ga/convulsao/solution1.csv
-./ga_setpack.py --k='5' --dt='3' --generation_size='50' --time='7200' --seed='5' solutions/ga/convulsao/solution2.csv
-./ga_setpack.py --k='5' --dt='3' --generation_size='50' --time='7200' --seed='10' solutions/ga/convulsao/solution3.csv
-./ga_setpack.py --k='5' --dt='3' --generation_size='50' --time='7200' --seed='20' solutions/ga/convulsao/solution4.csv
-./ga_setpack.py --k='5' --dt='3' --generation_size='50' --time='7200' --seed='50' solutions/ga/convulsao/solution5.csv
-./ga_setpack.py --k='5' --dt='3' --generation_size='50' --time='7200' --seed='80' solutions/ga/convulsao/solution6.csv
-./ga_setpack.py --k='5' --dt='3' --generation_size='50' --time='7200' --seed='100' solutions/ga/convulsao/solution7.csv
-./ga_setpack.py --k='5' --dt='3' --generation_size='50' --time='7200' --seed='150' solutions/ga/convulsao/solution8.csv
-./ga_setpack.py --k='5' --dt='3' --generation_size='50' --time='7200' --seed='200' solutions/ga/convulsao/solution9.csv
-./ga_setpack.py --k='5' --dt='3' --generation_size='50' --time='7200' --seed='1000' solutions/ga/convulsao/solution10.csv
+./ga_setpack.py --k='5' --dt='3' --generation_size='50' --maxs='50' --time='7200' --seed='1' solutions/ga/convulsao/solution1.csv
+./ga_setpack.py --k='5' --dt='3' --generation_size='50' --maxs='50' --time='7200' --seed='5' solutions/ga/convulsao/solution2.csv
+./ga_setpack.py --k='5' --dt='3' --generation_size='50' --maxs='50' --time='7200' --seed='10' solutions/ga/convulsao/solution3.csv
+./ga_setpack.py --k='5' --dt='3' --generation_size='50' --maxs='50' --time='7200' --seed='20' solutions/ga/convulsao/solution4.csv
+./ga_setpack.py --k='5' --dt='3' --generation_size='50' --maxs='50' --time='7200' --seed='50' solutions/ga/convulsao/solution5.csv
+./ga_setpack.py --k='5' --dt='3' --generation_size='50' --maxs='50' --time='7200' --seed='80' solutions/ga/convulsao/solution6.csv
+./ga_setpack.py --k='5' --dt='3' --generation_size='50' --maxs='50' --time='7200' --seed='100' solutions/ga/convulsao/solution7.csv
+./ga_setpack.py --k='5' --dt='3' --generation_size='50' --maxs='50' --time='7200' --seed='150' solutions/ga/convulsao/solution8.csv
+./ga_setpack.py --k='5' --dt='3' --generation_size='50' --maxs='50' --time='7200' --seed='200' solutions/ga/convulsao/solution9.csv
+./ga_setpack.py --k='5' --dt='3' --generation_size='50' --maxs='50' --time='7200' --seed='1000' solutions/ga/convulsao/solution10.csv
 ./vns_setpack.py --dt='2' --time='7200' --seed='1' solutions/vns/moba/solution1.csv
 ./vns_setpack.py --dt='2' --time='7200' --seed='5' solutions/vns/moba/solution2.csv
 ./vns_setpack.py --dt='2' --time='7200' --seed='10' solutions/vns/moba/solution3.csv
@@ -29,16 +29,16 @@
 ./vns_setpack.py --dt='2' --time='7200' --seed='150' solutions/vns/moba/solution8.csv
 ./vns_setpack.py --dt='2' --time='7200' --seed='200' solutions/vns/moba/solution9.csv
 ./vns_setpack.py --dt='2' --time='7200' --seed='1000' solutions/vns/moba/solution10.csv
-./vns_setpack.py --k='5' --dt='3' --time='7200' --seed='1' solutions/vns/convulsao/solution1.csv
-./vns_setpack.py --k='5' --dt='3' --time='7200' --seed='5' solutions/vns/convulsao/solution2.csv
-./vns_setpack.py --k='5' --dt='3' --time='7200' --seed='10' solutions/vns/convulsao/solution3.csv
-./vns_setpack.py --k='5' --dt='3' --time='7200' --seed='20' solutions/vns/convulsao/solution4.csv
-./vns_setpack.py --k='5' --dt='3' --time='7200' --seed='50' solutions/vns/convulsao/solution5.csv
-./vns_setpack.py --k='5' --dt='3' --time='7200' --seed='80' solutions/vns/convulsao/solution6.csv
-./vns_setpack.py --k='5' --dt='3' --time='7200' --seed='100' solutions/vns/convulsao/solution7.csv
-./vns_setpack.py --k='5' --dt='3' --time='7200' --seed='150' solutions/vns/convulsao/solution8.csv
-./vns_setpack.py --k='5' --dt='3' --time='7200' --seed='200' solutions/vns/convulsao/solution9.csv
-./vns_setpack.py --k='5' --dt='3' --time='7200' --seed='1000' solutions/vns/convulsao/solution10.csv
+./vns_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='1' solutions/vns/convulsao/solution1.csv
+./vns_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='5' solutions/vns/convulsao/solution2.csv
+./vns_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='10' solutions/vns/convulsao/solution3.csv
+./vns_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='20' solutions/vns/convulsao/solution4.csv
+./vns_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='50' solutions/vns/convulsao/solution5.csv
+./vns_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='80' solutions/vns/convulsao/solution6.csv
+./vns_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='100' solutions/vns/convulsao/solution7.csv
+./vns_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='150' solutions/vns/convulsao/solution8.csv
+./vns_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='200' solutions/vns/convulsao/solution9.csv
+./vns_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='1000' solutions/vns/convulsao/solution10.csv
 ./ils_setpack.py --dt='2' --time='7200' --seed='1' solutions/ils/moba/solution1.csv
 ./ils_setpack.py --dt='2' --time='7200' --seed='5' solutions/ils/moba/solution2.csv
 ./ils_setpack.py --dt='2' --time='7200' --seed='10' solutions/ils/moba/solution3.csv
@@ -49,16 +49,16 @@
 ./ils_setpack.py --dt='2' --time='7200' --seed='150' solutions/ils/moba/solution8.csv
 ./ils_setpack.py --dt='2' --time='7200' --seed='200' solutions/ils/moba/solution9.csv
 ./ils_setpack.py --dt='2' --time='7200' --seed='1000' solutions/ils/moba/solution10.csv
-./ils_setpack.py --k='5' --dt='3' --time='7200' --seed='1' solutions/ils/convulsao/solution1.csv
-./ils_setpack.py --k='5' --dt='3' --time='7200' --seed='5' solutions/ils/convulsao/solution2.csv
-./ils_setpack.py --k='5' --dt='3' --time='7200' --seed='10' solutions/ils/convulsao/solution3.csv
-./ils_setpack.py --k='5' --dt='3' --time='7200' --seed='20' solutions/ils/convulsao/solution4.csv
-./ils_setpack.py --k='5' --dt='3' --time='7200' --seed='50' solutions/ils/convulsao/solution5.csv
-./ils_setpack.py --k='5' --dt='3' --time='7200' --seed='80' solutions/ils/convulsao/solution6.csv
-./ils_setpack.py --k='5' --dt='3' --time='7200' --seed='100' solutions/ils/convulsao/solution7.csv
-./ils_setpack.py --k='5' --dt='3' --time='7200' --seed='150' solutions/ils/convulsao/solution8.csv
-./ils_setpack.py --k='5' --dt='3' --time='7200' --seed='200' solutions/ils/convulsao/solution9.csv
-./ils_setpack.py --k='5' --dt='3' --time='7200' --seed='1000' solutions/ils/convulsao/solution10.csv
+./ils_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='1' solutions/ils/convulsao/solution1.csv
+./ils_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='5' solutions/ils/convulsao/solution2.csv
+./ils_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='10' solutions/ils/convulsao/solution3.csv
+./ils_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='20' solutions/ils/convulsao/solution4.csv
+./ils_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='50' solutions/ils/convulsao/solution5.csv
+./ils_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='80' solutions/ils/convulsao/solution6.csv
+./ils_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='100' solutions/ils/convulsao/solution7.csv
+./ils_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='150' solutions/ils/convulsao/solution8.csv
+./ils_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='200' solutions/ils/convulsao/solution9.csv
+./ils_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='1000' solutions/ils/convulsao/solution10.csv
 ./grasp_fs_setpack.py --dt='2' --time='7200' --seed='1' solutions/grasp/moba/solution1.csv
 ./grasp_fs_setpack.py --dt='2' --time='7200' --seed='5' solutions/grasp/moba/solution2.csv
 ./grasp_fs_setpack.py --dt='2' --time='7200' --seed='10' solutions/grasp/moba/solution3.csv
@@ -69,16 +69,16 @@
 ./grasp_fs_setpack.py --dt='2' --time='7200' --seed='150' solutions/grasp/moba/solution8.csv
 ./grasp_fs_setpack.py --dt='2' --time='7200' --seed='200' solutions/grasp/moba/solution9.csv
 ./grasp_fs_setpack.py --dt='2' --time='7200' --seed='1000' solutions/grasp/moba/solution10.csv
-./grasp_fs_setpack.py --k='5' --dt='3' --time='7200' --seed='1' solutions/grasp/convulsao/solution1.csv
-./grasp_fs_setpack.py --k='5' --dt='3' --time='7200' --seed='5' solutions/grasp/convulsao/solution2.csv
-./grasp_fs_setpack.py --k='5' --dt='3' --time='7200' --seed='10' solutions/grasp/convulsao/solution3.csv
-./grasp_fs_setpack.py --k='5' --dt='3' --time='7200' --seed='20' solutions/grasp/convulsao/solution4.csv
-./grasp_fs_setpack.py --k='5' --dt='3' --time='7200' --seed='50' solutions/grasp/convulsao/solution5.csv
-./grasp_fs_setpack.py --k='5' --dt='3' --time='7200' --seed='80' solutions/grasp/convulsao/solution6.csv
-./grasp_fs_setpack.py --k='5' --dt='3' --time='7200' --seed='100' solutions/grasp/convulsao/solution7.csv
-./grasp_fs_setpack.py --k='5' --dt='3' --time='7200' --seed='150' solutions/grasp/convulsao/solution8.csv
-./grasp_fs_setpack.py --k='5' --dt='3' --time='7200' --seed='200' solutions/grasp/convulsao/solution9.csv
-./grasp_fs_setpack.py --k='5' --dt='3' --time='7200' --seed='1000' solutions/grasp/convulsao/solution10.csv
+./grasp_fs_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='1' solutions/grasp/convulsao/solution1.csv
+./grasp_fs_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='5' solutions/grasp/convulsao/solution2.csv
+./grasp_fs_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='10' solutions/grasp/convulsao/solution3.csv
+./grasp_fs_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='20' solutions/grasp/convulsao/solution4.csv
+./grasp_fs_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='50' solutions/grasp/convulsao/solution5.csv
+./grasp_fs_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='80' solutions/grasp/convulsao/solution6.csv
+./grasp_fs_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='100' solutions/grasp/convulsao/solution7.csv
+./grasp_fs_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='150' solutions/grasp/convulsao/solution8.csv
+./grasp_fs_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='200' solutions/grasp/convulsao/solution9.csv
+./grasp_fs_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='1000' solutions/grasp/convulsao/solution10.csv
 ./vns_setpack.py --invert='1' --dt='2' --time='7200' --seed='1' solutions/vns/moba/invsolution1.csv
 ./vns_setpack.py --invert='1' --dt='2' --time='7200' --seed='5' solutions/vns/moba/invsolution2.csv
 ./vns_setpack.py --invert='1' --dt='2' --time='7200' --seed='10' solutions/vns/moba/invsolution3.csv
@@ -89,15 +89,15 @@
 ./vns_setpack.py --invert='1' --dt='2' --time='7200' --seed='150' solutions/vns/moba/invsolution8.csv
 ./vns_setpack.py --invert='1' --dt='2' --time='7200' --seed='200' solutions/vns/moba/invsolution9.csv
 ./vns_setpack.py --invert='1' --dt='2' --time='7200' --seed='1000' solutions/vns/moba/invsolution10.csv
-./vns_setpack.py --k='5' --invert='1' --dt='3' --time='7200' --seed='1' solutions/vns/convulsao/invsolution1.csv
-./vns_setpack.py --k='5' --invert='1' --dt='3' --time='7200' --seed='5' solutions/vns/convulsao/invsolution2.csv
-./vns_setpack.py --k='5' --invert='1' --dt='3' --time='7200' --seed='10' solutions/vns/convulsao/invsolution3.csv
-./vns_setpack.py --k='5' --invert='1' --dt='3' --time='7200' --seed='20' solutions/vns/convulsao/invsolution4.csv
-./vns_setpack.py --k='5' --invert='1' --dt='3' --time='7200' --seed='50' solutions/vns/convulsao/invsolution5.csv
-./vns_setpack.py --k='5' --invert='1' --dt='3' --time='7200' --seed='80' solutions/vns/convulsao/invsolution6.csv
-./vns_setpack.py --k='5' --invert='1' --dt='3' --time='7200' --seed='100' solutions/vns/convulsao/invsolution7.csv
-./vns_setpack.py --k='5' --invert='1' --dt='3' --time='7200' --seed='150' solutions/vns/convulsao/invsolution8.csv
-./vns_setpack.py --k='5' --invert='1' --dt='3' --time='7200' --seed='200' solutions/vns/convulsao/invsolution9.csv
-./vns_setpack.py --k='5' --invert='1' --dt='3' --time='7200' --seed='1000' solutions/vns/convulsao/invsolution10.csv
+./vns_setpack.py --k='5' --invert='1' --dt='3' --maxs='50' --time='7200' --seed='1' solutions/vns/convulsao/invsolution1.csv
+./vns_setpack.py --k='5' --invert='1' --dt='3' --maxs='50' --time='7200' --seed='5' solutions/vns/convulsao/invsolution2.csv
+./vns_setpack.py --k='5' --invert='1' --dt='3' --maxs='50' --time='7200' --seed='10' solutions/vns/convulsao/invsolution3.csv
+./vns_setpack.py --k='5' --invert='1' --dt='3' --maxs='50' --time='7200' --seed='20' solutions/vns/convulsao/invsolution4.csv
+./vns_setpack.py --k='5' --invert='1' --dt='3' --maxs='50' --time='7200' --seed='50' solutions/vns/convulsao/invsolution5.csv
+./vns_setpack.py --k='5' --invert='1' --dt='3' --maxs='50' --time='7200' --seed='80' solutions/vns/convulsao/invsolution6.csv
+./vns_setpack.py --k='5' --invert='1' --dt='3' --maxs='50' --time='7200' --seed='100' solutions/vns/convulsao/invsolution7.csv
+./vns_setpack.py --k='5' --invert='1' --dt='3' --maxs='50' --time='7200' --seed='150' solutions/vns/convulsao/invsolution8.csv
+./vns_setpack.py --k='5' --invert='1' --dt='3' --maxs='50' --time='7200' --seed='200' solutions/vns/convulsao/invsolution9.csv
+./vns_setpack.py --k='5' --invert='1' --dt='3' --maxs='50' --time='7200' --seed='1000' solutions/vns/convulsao/invsolution10.csv
 
 
