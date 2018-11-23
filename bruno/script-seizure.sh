50a2b42a43c932d9e5b83764f9761edef2b6d1df
Marcos Ribeiro <mhfribeiro@gmail.com>
2018-11-23 16:38:33 -0200

    Resultados - convulsão: primeira metade

--- bruno/script-seizure.sh
+++ bruno/script-seizure.sh
@@ -1,58 +1,58 @@
 #!/bin/bash
 #### seizure
 #GA
-./ga_setpack.py --k='5' --dt='3' --generation_size='50' --maxs='50' --time='7200' --seed='1' solutions-new/seizure/ga/solution1.csv
-./ga_setpack.py --k='5' --dt='3' --generation_size='50' --maxs='50' --time='7200' --seed='5' solutions-new/seizure/ga/solution2.csv
-./ga_setpack.py --k='5' --dt='3' --generation_size='50' --maxs='50' --time='7200' --seed='10' solutions-new/seizure/ga/solution3.csv
-./ga_setpack.py --k='5' --dt='3' --generation_size='50' --maxs='50' --time='7200' --seed='20' solutions-new/seizure/ga/solution4.csv
-./ga_setpack.py --k='5' --dt='3' --generation_size='50' --maxs='50' --time='7200' --seed='50' solutions-new/seizure/ga/solution5.csv
-./ga_setpack.py --k='5' --dt='3' --generation_size='50' --maxs='50' --time='7200' --seed='80' solutions-new/seizure/ga/solution6.csv
-./ga_setpack.py --k='5' --dt='3' --generation_size='50' --maxs='50' --time='7200' --seed='100' solutions-new/seizure/ga/solution7.csv
-./ga_setpack.py --k='5' --dt='3' --generation_size='50' --maxs='50' --time='7200' --seed='150' solutions-new/seizure/ga/solution8.csv
-./ga_setpack.py --k='5' --dt='3' --generation_size='50' --maxs='50' --time='7200' --seed='200' solutions-new/seizure/ga/solution9.csv
-./ga_setpack.py --k='5' --dt='3' --generation_size='50' --maxs='50' --time='7200' --seed='1000' solutions-new/seizure/ga/solution10.csv
+#./ga_setpack.py --k='5' --dt='3' --generation_size='50' --maxs='50' --time='7200' --seed='1' -crth='0.90' solutions-new/seizure/ga/solution1.csv
+#./ga_setpack.py --k='5' --dt='3' --generation_size='50' --maxs='50' --time='7200' --seed='5' -crth='0.90' solutions-new/seizure/ga/solution2.csv
+#./ga_setpack.py --k='5' --dt='3' --generation_size='50' --maxs='50' --time='7200' --seed='10' -crth='0.90' solutions-new/seizure/ga/solution3.csv
+#./ga_setpack.py --k='5' --dt='3' --generation_size='50' --maxs='50' --time='7200' --seed='20' -crth='0.90' solutions-new/seizure/ga/solution4.csv
+#./ga_setpack.py --k='5' --dt='3' --generation_size='50' --maxs='50' --time='7200' --seed='50' -crth='0.90' solutions-new/seizure/ga/solution5.csv
+#./ga_setpack.py --k='5' --dt='3' --generation_size='50' --maxs='50' --time='7200' --seed='80' -crth='0.90' solutions-new/seizure/ga/solution6.csv
+#./ga_setpack.py --k='5' --dt='3' --generation_size='50' --maxs='50' --time='7200' --seed='100' -crth='0.90' solutions-new/seizure/ga/solution7.csv
+#./ga_setpack.py --k='5' --dt='3' --generation_size='50' --maxs='50' --time='7200' --seed='150' -crth='0.90' solutions-new/seizure/ga/solution8.csv
+#./ga_setpack.py --k='5' --dt='3' --generation_size='50' --maxs='50' --time='7200' --seed='200' -crth='0.90' solutions-new/seizure/ga/solution9.csv
+#./ga_setpack.py --k='5' --dt='3' --generation_size='50' --maxs='50' --time='7200' --seed='1000' -crth='0.90' solutions-new/seizure/ga/solution10.csv
 #VNS
-./vns_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='1' solutions-new/seizure/vns/solution1.csv
-./vns_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='5' solutions-new/seizure/vns/solution2.csv
-./vns_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='10' solutions-new/seizure/vns/solution3.csv
-./vns_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='20' solutions-new/seizure/vns/solution4.csv
-./vns_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='50' solutions-new/seizure/vns/solution5.csv
-./vns_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='80' solutions-new/seizure/vns/solution6.csv
-./vns_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='100' solutions-new/seizure/vns/solution7.csv
-./vns_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='150' solutions-new/seizure/vns/solution8.csv
-./vns_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='200' solutions-new/seizure/vns/solution9.csv
-./vns_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='1000' solutions-new/seizure/vns/solution10.csv
+#./vns_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='1' -crth='0.90' solutions-new/seizure/vns/solution1.csv
+#./vns_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='5' -crth='0.90' solutions-new/seizure/vns/solution2.csv
+#./vns_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='10' -crth='0.90' solutions-new/seizure/vns/solution3.csv
+#./vns_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='20' -crth='0.90' solutions-new/seizure/vns/solution4.csv
+#./vns_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='50' -crth='0.90' solutions-new/seizure/vns/solution5.csv
+#./vns_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='80' -crth='0.90' solutions-new/seizure/vns/solution6.csv
+#./vns_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='100' -crth='0.90' solutions-new/seizure/vns/solution7.csv
+#./vns_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='150' -crth='0.90' solutions-new/seizure/vns/solution8.csv
+#./vns_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='200' -crth='0.90' solutions-new/seizure/vns/solution9.csv
+#./vns_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='1000' -crth='0.90' solutions-new/seizure/vns/solution10.csv
 #ILS
-./ils_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='1' solutions-new/seizure/ils/solution1.csv
-./ils_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='5' solutions-new/seizure/ils/solution2.csv
-./ils_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='10' solutions-new/seizure/ils/solution3.csv
-./ils_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='20' solutions-new/seizure/ils/solution4.csv
-./ils_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='50' solutions-new/seizure/ils/solution5.csv
-./ils_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='80' solutions-new/seizure/ils/solution6.csv
-./ils_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='100' solutions-new/seizure/ils/solution7.csv
-./ils_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='150' solutions-new/seizure/ils/solution8.csv
-./ils_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='200' solutions-new/seizure/ils/solution9.csv
-./ils_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='1000' solutions-new/seizure/ils/solution10.csv
+#./ils_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='1' -crth='0.90' solutions-new/seizure/ils/solution1.csv
+#./ils_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='5' -crth='0.90' solutions-new/seizure/ils/solution2.csv
+#./ils_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='10' -crth='0.90' solutions-new/seizure/ils/solution3.csv
+#./ils_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='20' -crth='0.90' solutions-new/seizure/ils/solution4.csv
+#./ils_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='50' -crth='0.90' solutions-new/seizure/ils/solution5.csv
+./ils_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='80' -crth='0.90' solutions-new/seizure/ils/solution6.csv
+./ils_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='100' -crth='0.90' solutions-new/seizure/ils/solution7.csv
+./ils_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='150' -crth='0.90' solutions-new/seizure/ils/solution8.csv
+./ils_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='200' -crth='0.90' solutions-new/seizure/ils/solution9.csv
+./ils_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='1000' -crth='0.90' solutions-new/seizure/ils/solution10.csv
 #GRASP
-./grasp_fs_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='1' solutions-new/seizure/grasp/solution1.csv
-./grasp_fs_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='5' solutions-new/seizure/grasp/solution2.csv
-./grasp_fs_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='10' solutions-new/seizure/grasp/solution3.csv
-./grasp_fs_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='20' solutions-new/seizure/grasp/solution4.csv
-./grasp_fs_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='50' solutions-new/seizure/grasp/solution5.csv
-./grasp_fs_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='80' solutions-new/seizure/grasp/solution6.csv
-./grasp_fs_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='100' solutions-new/seizure/grasp/solution7.csv
-./grasp_fs_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='150' solutions-new/seizure/grasp/solution8.csv
-./grasp_fs_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='200' solutions-new/seizure/grasp/solution9.csv
-./grasp_fs_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='1000' solutions-new/seizure/grasp/solution10.csv
+./grasp_fs_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='1' -crth='0.90' solutions-new/seizure/grasp/solution1.csv
+./grasp_fs_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='5' -crth='0.90' solutions-new/seizure/grasp/solution2.csv
+./grasp_fs_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='10' -crth='0.90' solutions-new/seizure/grasp/solution3.csv
+./grasp_fs_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='20' -crth='0.90' solutions-new/seizure/grasp/solution4.csv
+./grasp_fs_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='50' -crth='0.90' solutions-new/seizure/grasp/solution5.csv
+./grasp_fs_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='80' -crth='0.90' solutions-new/seizure/grasp/solution6.csv
+./grasp_fs_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='100' -crth='0.90' solutions-new/seizure/grasp/solution7.csv
+./grasp_fs_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='150' -crth='0.90' solutions-new/seizure/grasp/solution8.csv
+./grasp_fs_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='200' -crth='0.90' solutions-new/seizure/grasp/solution9.csv
+./grasp_fs_setpack.py --k='5' --dt='3' --maxs='50' --time='7200' --seed='1000' -crth='0.90' solutions-new/seizure/grasp/solution10.csv
 #VNS-INV
-./vns_setpack.py --k='5' --invert='1' --dt='3' --maxs='50' --time='7200' --seed='1' solutions-new/seizure/vns-inv/solution1.csv
-./vns_setpack.py --k='5' --invert='1' --dt='3' --maxs='50' --time='7200' --seed='5' solutions-new/seizure/vns-inv/solution2.csv
-./vns_setpack.py --k='5' --invert='1' --dt='3' --maxs='50' --time='7200' --seed='10' solutions-new/seizure/vns-inv/solution3.csv
-./vns_setpack.py --k='5' --invert='1' --dt='3' --maxs='50' --time='7200' --seed='20' solutions-new/seizure/vns-inv/solution4.csv
-./vns_setpack.py --k='5' --invert='1' --dt='3' --maxs='50' --time='7200' --seed='50' solutions-new/seizure/vns-inv/solution5.csv
-./vns_setpack.py --k='5' --invert='1' --dt='3' --maxs='50' --time='7200' --seed='80' solutions-new/seizure/vns-inv/solution6.csv
-./vns_setpack.py --k='5' --invert='1' --dt='3' --maxs='50' --time='7200' --seed='100' solutions-new/seizure/vns-inv/solution7.csv
-./vns_setpack.py --k='5' --invert='1' --dt='3' --maxs='50' --time='7200' --seed='150' solutions-new/seizure/vns-inv/solution8.csv
-./vns_setpack.py --k='5' --invert='1' --dt='3' --maxs='50' --time='7200' --seed='200' solutions-new/seizure/vns-inv/solution9.csv
-./vns_setpack.py --k='5' --invert='1' --dt='3' --maxs='50' --time='7200' --seed='1000' solutions-new/seizure/vns-inv/solution10.csv
+./vns_setpack.py --k='5' --invert='1' --dt='3' --maxs='50' --time='7200' --seed='1' -crth='0.90' solutions-new/seizure/vns-inv/solution1.csv
+./vns_setpack.py --k='5' --invert='1' --dt='3' --maxs='50' --time='7200' --seed='5' -crth='0.90' solutions-new/seizure/vns-inv/solution2.csv
+./vns_setpack.py --k='5' --invert='1' --dt='3' --maxs='50' --time='7200' --seed='10' -crth='0.90' solutions-new/seizure/vns-inv/solution3.csv
+./vns_setpack.py --k='5' --invert='1' --dt='3' --maxs='50' --time='7200' --seed='20' -crth='0.90' solutions-new/seizure/vns-inv/solution4.csv
+./vns_setpack.py --k='5' --invert='1' --dt='3' --maxs='50' --time='7200' --seed='50' -crth='0.90' solutions-new/seizure/vns-inv/solution5.csv
+./vns_setpack.py --k='5' --invert='1' --dt='3' --maxs='50' --time='7200' --seed='80' -crth='0.90' solutions-new/seizure/vns-inv/solution6.csv
+./vns_setpack.py --k='5' --invert='1' --dt='3' --maxs='50' --time='7200' --seed='100' -crth='0.90' solutions-new/seizure/vns-inv/solution7.csv
+./vns_setpack.py --k='5' --invert='1' --dt='3' --maxs='50' --time='7200' --seed='150' -crth='0.90' solutions-new/seizure/vns-inv/solution8.csv
+./vns_setpack.py --k='5' --invert='1' --dt='3' --maxs='50' --time='7200' --seed='200' -crth='0.90' solutions-new/seizure/vns-inv/solution9.csv
+./vns_setpack.py --k='5' --invert='1' --dt='3' --maxs='50' --time='7200' --seed='1000' -crth='0.90' solutions-new/seizure/vns-inv/solution10.csv
 
